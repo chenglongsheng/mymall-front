d95aec0b4025db6012c2fd605e42782f07b502ae
成龙生 <1536463948@qq.com>
2021-11-28 22:35:47 +0800

    展示三级分类

---
+++ renren-fast-vue/src/views/modules/product/category.vue
@@ -0,0 +1,42 @@
+<template>
+  <el-tree
+    :data="menus"
+    :props="defaultProps"
+    @node-click="handleNodeClick"
+  ></el-tree>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      menus: [],
+      defaultProps: {
+        children: 'children',
+        label: 'name'
+      }
+    }
+  },
+  created() {
+    this.getCategory()
+  },
+  methods: {
+    getCategory() {
+      this.$http({
+        url: this.$http.adornUrl('/product/category/list/tree'),
+        method: 'get'
+      }).then(({ data }) => {
+        // 对象解构
+        // console.log('success', data.data)
+        this.menus = data.data
+      })
+    },
+    handleNodeClick(data) {
+      console.log(data)
+    }
+  }
+}
+</script>
+
+<style>
+</style>
