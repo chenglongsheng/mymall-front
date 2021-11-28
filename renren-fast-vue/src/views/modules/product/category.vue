c2fd92cd9e068bb5da2f74642674646ffaa4231c
成龙生 <1536463948@qq.com>
2021-11-28 23:02:38 +0800

    选择样式

--- renren-fast-vue/src/views/modules/product/category.vue
+++ renren-fast-vue/src/views/modules/product/category.vue
@@ -1,9 +1,33 @@
 <template>
   <el-tree
+    node-key="catId"
     :data="menus"
     :props="defaultProps"
-    @node-click="handleNodeClick"
-  ></el-tree>
+    show-checkbox
+    :expand-on-click-node="false"
+  >
+    <span class="custom-tree-node" slot-scope="{ node, data }">
+      <span>{{ node.label }}</span>
+      <span>
+        <el-button
+          v-if="node.level <= 2"
+          type="text"
+          size="mini"
+          @click="() => append(data)"
+        >
+          Append
+        </el-button>
+        <el-button
+          v-if="node.isLeaf"
+          type="text"
+          size="mini"
+          @click="() => remove(node, data)"
+        >
+          Delete
+        </el-button>
+      </span>
+    </span></el-tree
+  >
 </template>
 
 <script>
@@ -31,12 +55,23 @@ export default {
         this.menus = data.data
       })
     },
-    handleNodeClick(data) {
-      console.log(data)
+    append(data) {
+      console.log('append', data)
+    },
+    remove(node, data) {
+      console.log('remove', node, data)
     }
   }
 }
 </script>
 
 <style>
+.custom-tree-node {
+  flex: 1;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  font-size: 14px;
+  padding-right: 8px;
+}
 </style>
