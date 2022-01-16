53de38beae29ec47c6ce0a6a3a4d329799563e8d
成龙生 <1536463948@qq.com>
2022-01-16 16:50:07 +0800

    属性分组

---
+++ renren-fast-vue/src/views/modules/common/category.vue
@@ -0,0 +1,35 @@
+<template>
+  <el-tree :data="menus" :props="defaultProps" ref="tree"></el-tree>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      menus: [],
+      defaultProps: {
+        children: 'children',
+        label: 'name'
+      }
+    }
+  },
+  created() {
+    this.getCategory()
+  },
+  methods: {
+    getCategory() {
+      this.$http({
+        url: this.$http.adornUrl('/product/category/list/tree'),
+        method: 'get'
+      }).then(({ data }) => {
+        // 对象解构
+        // console.log('success', data.data)
+        this.menus = data.data
+      })
+    }
+  }
+}
+</script>
+
+<style>
+</style>
---
+++ renren-fast-vue/src/views/modules/product/attrgroup.vue
@@ -0,0 +1,20 @@
+<template>
+  <el-row :gutter="20">
+    <el-col :span="6">
+      <category></category>
+    </el-col>
+    <el-col :span="18">asdada</el-col>
+  </el-row>
+</template>
+
+<script>
+import Category from '../common/category.vue'
+export default {
+  components: { Category },
+  data() {
+    return {}
+  },
+  created() {},
+  methods: {}
+}
+</script>
